c1d05e3392a0df6168871430e3996971869651c1
Doston Sulaymon <dostonsulaymon@Dostons-MacBook-Pro.local>
2025-01-22 16:47:07 +0500

    ParseIntPipe added to make sure id is coming as number not string

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import { AppModule } from './app.module';
 import * as process from 'node:process';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import morgan from 'morgan';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.use(morgan('dev'));
+  // Apply global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Use 'dev' format for development
+
+  // Setup Swagger API documentation
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
@@ -17,11 +24,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document); // Change to '/docs' if needed
 
+  // Start the application
   await app.listen(PORT);
+  console.log(`Server started on port ${PORT}`);
 }
 
-bootstrap().then(() => {
-  console.log('Server started on port', PORT);
-});
+bootstrap();
