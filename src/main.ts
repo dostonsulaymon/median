272b863970b2cdbdd49f26b833f7da6293c2395e
Doston Sulaymon <dostonsulaymon@Dostons-MacBook-Pro.local>
2025-01-22 16:25:45 +0500

    Validion packages added and validation added to CreateArticleDTO

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
